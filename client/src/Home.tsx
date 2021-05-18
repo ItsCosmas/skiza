83cb18f16ddcf0eee98e073ef3361ee3ad2360e0
Cozy <devcosmas@gmail.com>
2021-05-18 14:49:11 +0300

    🚀 Initial Commit: Frontend Work

---
+++ client/src/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './components/nav/Nav';
+
+function Home() {
+	return (
+		<div className='home'>
+			<Nav />
+			<main className='main'>Main is Here</main>
+		</div>
+	);
+}
+
+export default Home;
---
+++ client/src/components/nav/Nav.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Nav() {
+	return <nav className='nav'>Nav Goes Here</nav>;
+}
+
+export default Nav;
