3947fb47ea62e79e668d6057c27525c568a4b8ce
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2024-10-02 12:01:08 +0300

    Use Vite and Daisy UI

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,15 +1,10 @@
-import React from 'react';
 import Nav from 'components/nav/Nav';
-import Table from 'views/Table/Table';
-
-import './Home.scss';
 
 function Home() {
 	return (
 		<div>
 			<Nav />
 			<main>
-				<Table />
 			</main>
 		</div>
 	);
