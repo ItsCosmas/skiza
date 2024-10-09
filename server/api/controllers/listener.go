539912916da9a0b416495474e4dac236003dae70
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2024-10-09 20:25:35 +0300

    render json formattable messages

--- server/api/controllers/listener.go
+++ server/api/controllers/listener.go
@@ -1,10 +1,11 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
-	"strconv"
 
-	"math/rand/v2"
+	"skiza/api/schema"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -17,8 +18,35 @@ import (
 //	@Success		200	{object}	Response
 //	@Router			/listener [post]
 func ListenerController(c *fiber.Ctx, broadcast chan<- string) error {
-	response := HTTPResponse(http.StatusOK, "Callback Received", "Callback Body")
-	// Broadcast a message to all WebSocket clients
-	broadcast <- "A callback has been received: " + strconv.Itoa(rand.IntN(100))
-	return c.JSON(response)
+	httpResponse := HTTPResponse(http.StatusOK, "Callback Received", "Callback Body")
+
+	// Create an ExternalResponse object
+	responseBody := string(c.Body())
+	sourceIP := c.IP() // Get the source IP
+
+	response := schema.ExternalResponse{
+		ResponseBody: responseBody,
+		SourceIP:     sourceIP,
+	}
+
+	// Convert ExternalResponse to JSON (byte slice)
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).SendString("Error encoding response")
+	}
+
+	// return c.JSON(httpResponse)
+
+	c.JSON(httpResponse)
+
+	// Broadcast the ExternalResponse in a goroutine
+	go func() {
+		// Optionally log the body of the request
+		fmt.Println(responseBody)
+
+		// Broadcast the ExternalResponse to all WebSocket clients
+		broadcast <- string(jsonResponse)
+	}()
+
+	return nil // Return nil since we already sent the response
 }
